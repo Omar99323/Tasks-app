c9413b9954109942ff6d50b0413899b59033d606
Omar99323 <110248328+Omar99323@users.noreply.github.com>
2023-02-19 20:16:29 +0200

    Refactoring ..

--- lib/screens/home/home_screens.dart
+++ lib/screens/home/home_screens.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:to_do/models/task_model.dart';
 import 'package:to_do/screens/nav_screens/archived_tasks.dart';
 import 'package:to_do/screens/nav_screens/done_tasks.dart';
 import 'package:to_do/screens/nav_screens/new_tasks.dart';
-import 'package:to_do/widgets/custom_form_field.dart';
+import '../../widgets/custom_model_sheet.dart';
 
 List<Task> tsks = [];
 
@@ -166,94 +165,5 @@ class _HomeScreensState extends State<HomeScreens> {
     int count = await database
         .rawDelete('DELETE FROM Test WHERE name = ?', ['another name']);
     assert(count == 1);
-  }
-}
-
-class CustomBottomSheet extends StatelessWidget {
-  const CustomBottomSheet({
-    super.key,
-    required this.nameconroller,
-    required this.dateconroller,
-    required this.timeconroller,
-    required this.formkey,
-  });
-
-  final TextEditingController nameconroller;
-  final TextEditingController dateconroller;
-  final TextEditingController timeconroller;
-
-  final GlobalKey formkey;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey[100],
-      padding: const EdgeInsets.all(20),
-      child: Form(
-        key: formkey,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            CustomFormField(
-              controler: nameconroller,
-              icon: Icons.title,
-              label: 'Task Title',
-              type: TextInputType.text,
-              validator: (data) {
-                if (data!.isEmpty) {
-                  return 'Empty Title Field';
-                }
-              },
-              onsubmit: (data) {},
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            CustomFormField(
-              controler: dateconroller,
-              icon: Icons.calendar_today,
-              label: 'Task Date',
-              type: TextInputType.datetime,
-              validator: (p0) {
-                if (p0!.isEmpty) {
-                  return 'Empty Date Field';
-                }
-              },
-              onsubmit: (data) {},
-              ontap: () {
-                final f = DateFormat('yyyy-MM-dd');
-                showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime.now(),
-                  lastDate: DateTime.parse('2035-12-30'),
-                ).then((value) => dateconroller.text = f.format(value!));
-              },
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            CustomFormField(
-              controler: timeconroller,
-              icon: Icons.watch,
-              label: 'Task Time',
-              type: TextInputType.number,
-              validator: (p0) {
-                if (p0!.isEmpty) {
-                  return 'Empty Time Field';
-                }
-              },
-              onsubmit: (data) {},
-              ontap: () {
-                showTimePicker(
-                  context: context,
-                  initialTime: TimeOfDay.now(),
-                ).then((value) => timeconroller.text = value!.format(context));
-              },
-            ),
-          ],
-        ),
-      ),
-    );
   }
 }
---
+++ lib/widgets/custom_model_sheet.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'custom_form_field.dart';
+
+class CustomBottomSheet extends StatelessWidget {
+  const CustomBottomSheet({
+    super.key,
+    required this.nameconroller,
+    required this.dateconroller,
+    required this.timeconroller,
+    required this.formkey,
+  });
+
+  final TextEditingController nameconroller;
+  final TextEditingController dateconroller;
+  final TextEditingController timeconroller;
+
+  final GlobalKey formkey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[100],
+      padding: const EdgeInsets.all(20),
+      child: Form(
+        key: formkey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CustomFormField(
+              controler: nameconroller,
+              icon: Icons.title,
+              label: 'Task Title',
+              type: TextInputType.text,
+              validator: (data) {
+                if (data!.isEmpty) {
+                  return 'Empty Title Field';
+                }
+              },
+              onsubmit: (data) {},
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            CustomFormField(
+              controler: dateconroller,
+              icon: Icons.calendar_today,
+              label: 'Task Date',
+              type: TextInputType.datetime,
+              validator: (p0) {
+                if (p0!.isEmpty) {
+                  return 'Empty Date Field';
+                }
+              },
+              onsubmit: (data) {},
+              ontap: () {
+                final f = DateFormat('yyyy-MM-dd');
+                showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime.now(),
+                  lastDate: DateTime.parse('2035-12-30'),
+                ).then((value) => dateconroller.text = f.format(value!));
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            CustomFormField(
+              controler: timeconroller,
+              icon: Icons.watch,
+              label: 'Task Time',
+              type: TextInputType.number,
+              validator: (p0) {
+                if (p0!.isEmpty) {
+                  return 'Empty Time Field';
+                }
+              },
+              onsubmit: (data) {},
+              ontap: () {
+                showTimePicker(
+                  context: context,
+                  initialTime: TimeOfDay.now(),
+                ).then((value) => timeconroller.text = value!.format(context));
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
